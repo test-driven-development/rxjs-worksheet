6717572e4f2d1e1da430082af614af7999be0151
wpannell <wil.pannell@gmail.com>
2020-03-17 10:51:40 -0700

    correct forEach contract

--- src/index.js
+++ src/index.js
@@ -1,29 +1,24 @@
 import { keypresses$ } from './key-presses'
 import { searchWikipedia } from './search-wikipedia'
+import { Observable } from 'rxjs'
 
 keypresses$.forEach(k => console.log(k.keyCode))
 
 function getWikipediaSearchResults$() {
-  return {
-    forEach: function forEach(observer) {
-      let isCancelled = false
-      searchWikipedia().then(results => {
-        if (!isCancelled) {
-          observer.next(results)
-          observer.complete()
-        }
-      })
-
-      return {
-        dispose: function dispose() {
-          isCancelled = true
-        }
+  return Observable.create(function forEach(observer) {
+    let isCancelled = false
+    searchWikipedia().then(results => {
+      if (!isCancelled) {
+        observer.next(results)
+        observer.complete()
       }
+    })
+    return function dispose() {
+      isCancelled = true
     }
-  }
+  })
 }
 
-getWikipediaSearchResults$().forEach({
-  next: results => console.log(results),
-  complete: () => console.log('completed')
-})
+getWikipediaSearchResults$()
+  .forEach(results => console.log(results))
+  .then(() => console.log('completed'))
