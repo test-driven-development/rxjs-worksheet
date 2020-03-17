bb50e9761ea2215a9b959261aae66b2def6c3bd1
wpannell <wil.pannell@gmail.com>
2020-03-17 09:26:41 -0700

    implement observable contract

--- src/index.js
+++ src/index.js
@@ -2,4 +2,28 @@ import { keypresses$ } from './key-presses'
 import { searchWikipedia } from './search-wikipedia'
 
 keypresses$.forEach(k => console.log(k.keyCode))
-searchWikipedia().then(x => console.log(x))
+
+function getWikipediaSearchResults$() {
+  return {
+    forEach: function forEach(observer) {
+      let isCancelled = false
+      searchWikipedia().then(results => {
+        if (!isCancelled) {
+          observer.next(results)
+          observer.complete()
+        }
+      })
+
+      return {
+        dispose: function dispose() {
+          isCancelled = true
+        }
+      }
+    }
+  }
+}
+
+getWikipediaSearchResults$().forEach({
+  next: results => console.log(results),
+  complete: () => console.log('completed')
+})
