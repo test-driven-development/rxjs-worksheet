1161ff39d170f438e17de8516e42d92722c9c991
wpannell <wil.pannell@gmail.com>
2020-03-17 07:59:55 -0700

    introduce observable stream of keypresses

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
+import { fromEvent } from 'rxjs'
+
+const textbox = document.getElementById('textbox')
+const keypresses$ = fromEvent(textbox, 'keypress')
+keypresses$.forEach(k => console.log(k.keyCode))
+
 async function searchWikipedia() {
   const url =
-    'https://en.wikipedia.org/w/api.php?action=query&' +
-    'format=json&origin=*&list=search&srsearch=Terminator'
+    'https://en.wikipedia.org/w/api.php?' +
+    'action=query&format=json&origin=*' +
+    '&list=search&srsearch=Terminator'
 
   const response = await fetch(url)
-  if (response && response.status >= 200 && response.status < 400) {
-    const data = await response.json()
-    const result = data.query.search.map(s => s.title)
-    console.log(JSON.stringify(result))
-  } else {
-    console.log('error')
-  }
+  const data = await response.json()
+  const result = data.query.search.map(s => s.title)
+  console.log(JSON.stringify(result))
 }
 
 searchWikipedia()
